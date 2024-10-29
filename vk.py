8f504fea5ac28e838bc7fed789231d0ba8f31f75
N_Da <nadejdadawydowa25@gmail.com>
2024-10-29 20:06:10 +0300

    better

--- vk.py
+++ vk.py
@@ -3,6 +3,11 @@ from pprint import pprint
 import json
 
 
+def rank_sizes(letter):
+            sizes = ['s', 'm', 'x', 'o', 'p', 'q', 'r', 'y', 'z', 'w']
+            return sizes.index(letter)
+
+
 class VK:
     def __init__(self, access_token, user_id, version='5.131'):
         self.base_address = 'https://api.vk.com/method/'
@@ -10,20 +15,31 @@ class VK:
         self.id = user_id
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
-
-
-    def users_info(self):
-        url = f'{self.base_address}users.get'
-        params = {'user_ids': self.id}
-        response = requests.get(url, params={**self.params, **params})
-        return response.json()
     
-    def big_photos_get(self, count = 1):
+    def big_photos_get(self, count=5):
         url = f'{self.base_address}photos.get'
-        params = {'owner_id': self.id, 'album_id': 'profile', 'rev' : 1, 'extended': 1, 'count': 1}
+        params = {'owner_id': self.id, 'album_id': 'profile', 'rev' : 1, 'extended': 1, 'count': count}
         response = requests.get(url, params={**self.params, **params})
-        ##for photo in response.json()
-        return response.json()
+        photo_dict = {}
+        photos = [] ##json файл
+        
+        for photo in response.json()['response'] ['items']:
+            photo_info = {}
+            size = max(photo['sizes'], key=lambda x: rank_sizes(x['type']))
+            if photo['likes']['count'] not in photo_dict.keys():
+                photo_dict[photo['likes']['count']] = size['url']
+                photo_info['file_name'] = f"[photo['likes']['count']].jpg"
+            else:
+                photo_dict[f"{photo['likes']['count']} + {photo['date']}"] = size['url']
+                photo_info['file_name'] = f"{photo['likes']['count']} + {photo['date']}.jpg"
+
+            photo_info['size'] = size['type']
+            photos.append(photo_info)
+
+        with open("photos.json", "w") as file:
+            json.dump(photos, file, indent=4)
+        
+        return photo_dict
 
 
 
@@ -39,26 +55,26 @@ class YD:
         response = requests.put(url=url, headers=self.headers, params=params)
         return response
     
-    def upload(self, file_url):
+    def upload(self, file_url, file_name):
         url = f'{self.base_url}/v1/disk/resources/upload'
-        params = {'path': 'photosvk/milana.png'}
+        params = {'path': f'photovk/{file_name}.png'}
         href = requests.get(url=url, headers=self.headers, params=params).json().get('href')
         data = requests.get(url=file_url)
         requests.put(url=href, data=data, headers=self.headers)        
     
 
+##access_token = ''
+##user_id = ''
+##vk = VK(access_token, user_id)
+##token_yd = ''
 
-
-
-access_token = ''
-user_id = ''
-vk = VK(access_token, user_id)
-token_yd = ''
 yd = YD(token_yd)
-##yd.create_folder('photovk')
-pprint(vk.big_photos_get())
+yd.create_folder('photovk')
+photo_dict = vk.big_photos_get()
+for name, photo_url in photo_dict.items():
+    yd.upload(photo_url, name)
 
-##yd.upload(photo)
+
 
 
 
